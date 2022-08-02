bcddd432fe140dd423a8768026385b046ef784d2
DroidFreak32 <rushabshah32@gmail.com>
2022-08-02 20:23:01 +0530

    build-all: Remove unsupoprted architectures

--- _release/build-all.sh
+++ _release/build-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP_NAME="gdrive"
-PLATFORMS="darwin/386 darwin/amd64 darwin/arm darwin/arm64 dragonfly/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips64 linux/mips64le linux/rpi netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 openbsd/arm plan9/386 plan9/amd64 solaris/amd64 windows/386 windows/amd64"
+PLATFORMS="darwin/amd64 darwin/arm64 dragonfly/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le linux/mips64 linux/mips64le linux/rpi netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 openbsd/arm plan9/386 plan9/amd64 solaris/amd64 windows/386 windows/amd64"
 
 BIN_PATH="_release/bin"
 
